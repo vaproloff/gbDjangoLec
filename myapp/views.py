f94e1130518b004f599c13877b9a9b8b0f22c48b
Konstantin Pivnov <vaproloff@ya.ru>
2023-10-05 20:33:11 +0300

    Initial commit

---
+++ myapp/views.py
@@ -0,0 +1,21 @@
+import logging
+from django.http import HttpResponse
+
+logger = logging.getLogger(__name__)
+
+
+def index(request):
+    logger.info('Index page accessed')
+    return HttpResponse("Hello, world!")
+
+
+def about(request):
+    try:
+        # some code that might raise an exception
+        result = 1 / 0
+    except Exception as e:
+        logger.exception(f'Error in about page: {e}')
+        return HttpResponse("Oops, something went wrong.")
+    else:
+        logger.debug('About page accessed')
+        return HttpResponse("This is the about page.")
